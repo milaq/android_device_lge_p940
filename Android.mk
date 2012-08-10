f0d1f5d486173496333f12065d4c0bfb70b26046
Micha LaQua <micha.laqua@gmail.com>
2012-08-10 15:41:50 +0300

    add mkfilter

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2012 The Android Open-Source Project
+# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2011 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +17,7 @@
 
 # WARNING: Everything listed here will be built on ALL platforms,
 # including x86, the emulator, and the SDK.  Modules must be uniquely
-# named (liblights.tuna), and must build everywhere, or limit themselves
+# named (liblights.p940), and must build everywhere, or limit themselves
 # to only building on ARM if they include assembly. Individual makefiles
 # are responsible for having their own logic, for fine-grained control.
 
@@ -25,4 +26,10 @@ LOCAL_PATH := $(call my-dir)
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
+ifneq ($(filter p940,$(TARGET_DEVICE)),)
+
+ifneq ($(TARGET_SIMULATOR),true)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
+
+endif
