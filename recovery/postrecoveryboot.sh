d737fee739abb42ded666b9a41cad5e3042cead1
Micha LaQua <micha.laqua@gmail.com>
2012-08-13 16:06:32 +0300

    lets light it up and break it down

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,6 +1,11 @@
 #!/system/bin/sh
+# do it like mr. rhymes
+echo 255 > /sys/class/leds/button-backlight/brightness
+echo 100 > /sys/devices/platform/omap/omap_i2c.2/i2c-2/2-0038//brightness
+sleep 2
 # switch CPUfreq from performance to interactive & tune it
-sleep 60
 echo "interactive" >  /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
 echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+# remove recovery trigger if present
+rm /data/.recovery_mode
