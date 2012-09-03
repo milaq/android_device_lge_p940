76481ea0e783cbc5dc447a6d48b498d664a33f1c
Micha LaQua <micha.laqua@gmail.com>
2012-09-03 14:18:32 +0300

    fix typo

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_COPY_FILES := \
 	device/lge/p940/prebuilt/init.lgep940board.rc:root/init.lgep940board.rc \
 	device/lge/p940/prebuilt/ueventd.lgep940board.rc:root/ueventd.lgep940board.rc
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
 	device/lge/p940/init.p940.rc:root/init.p940.rc \
 	device/lge/p940/init.p940.usb.rc:root/init.p940.ril.rc \
 	device/lge/p940/init.p940.usb.rc:root/init.p940.usb.rc \
