7a4eb3487b9d0cc714cbe2d7629611ca02929b9c
milaq <micha.laqua@gmail.com>
2013-02-10 17:51:11 +0100

    update prebuilt kernel * support an additional 1.2GHz frequency step * rewrite power HAL to adapt to changed frequencies * initialise with 1GHz by default

--- power/power_p940.c
+++ power/power_p940.c
@@ -25,7 +25,15 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
+#define SCALINGMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+#define SCREENOFFMAXFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq"
 #define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
+
+#define MAX_BUF_SZ  10
+
+/* initialize freqs*/
+static char screen_off_max_freq[MAX_BUF_SZ] = "600000";
+static char scaling_max_freq[MAX_BUF_SZ] = "1008000";
 
 struct p940_power_module {
     struct power_module base;
@@ -55,6 +63,23 @@ static void sysfs_write(char *path, char *s)
     close(fd);
 }
 
+int sysfs_read(const char *path, char *buf, size_t size)
+{
+  int fd, len;
+
+  fd = open(path, O_RDONLY);
+  if (fd < 0)
+    return -1;
+
+  do {
+    len = read(fd, buf, size);
+  } while (len < 0 && errno == EINTR);
+
+  close(fd);
+
+  return len;
+}
+
 static void p940_power_init(struct power_module *module)
 {
     /*
@@ -72,6 +97,9 @@ static void p940_power_init(struct power_module *module)
                 "50");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay",
                 "100000");
+
+    sysfs_write(SCREENOFFMAXFREQ_PATH,
+                screen_off_max_freq);
 }
 
 static int boostpulse_open(struct p940_power_module *p940)
@@ -98,13 +126,27 @@ static int boostpulse_open(struct p940_power_module *p940)
 
 static void p940_power_set_interactive(struct power_module *module, int on)
 {
+    int len;
+
+    char buf[MAX_BUF_SZ];
+
     /*
      * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
      * cpufreq policy.
      */
+    if (!on) {
+        /* read the current scaling max freq and save it before updating */
+        len = sysfs_read(SCALINGMAXFREQ_PATH, buf, sizeof(buf));
 
-    sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? "1008000" : "600000");
+        if (len != -1)
+            memcpy(scaling_max_freq, buf, sizeof(buf));
+
+        sysfs_write(SCALINGMAXFREQ_PATH,
+                    on ? scaling_max_freq : screen_off_max_freq);
+    }
+    else
+        sysfs_write(SCALINGMAXFREQ_PATH,
+                    on ? scaling_max_freq : screen_off_max_freq);
 }
 
 static void p940_power_hint(struct power_module *module, power_hint_t hint,
