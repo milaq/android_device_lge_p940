781567cde71bfe8ab81cf6e10a6e2646b5c11163
milaq <micha.laqua@gmail.com>
2013-03-17 02:33:18 +0100

    fix up concurrent p2p support

--- full_p940.mk
+++ full_p940.mk
@@ -23,7 +23,7 @@
  
 # Torch
 PRODUCT_PACKAGES := \
-    Torch
+    Torch WiFiDirectDemo
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
