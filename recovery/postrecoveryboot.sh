4c0aed8a8000bfb073b83f26ee50d547de4544dc
milaq <micha.laqua@gmail.com>
2013-03-19 14:39:48 +0100

    recovery: use generic partition id

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 sleep 2
 ## Clear "boot-recovery...enable-wipe" flag
-dd if=/dev/zero of=/dev/block/mmcblk0p6 count=84 bs=1
+dd if=/dev/zero of=/dev/block/platform/omap/omap_hsmmc.1/by-name/misc count=84 bs=1
 # do it like mr. rhymes
 echo 255 > /sys/class/leds/button-backlight/brightness
 echo 100 > /sys/devices/platform/omap/omap_i2c.2/i2c-2/2-0038/brightness
