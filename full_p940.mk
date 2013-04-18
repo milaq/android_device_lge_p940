f45ae27ba2aab12787b361a70f63d0956410dbb8
Micha LaQua <micha.laqua@gmail.com>
2013-04-18 14:23:23 +0300

    let's not build the old crappy wifidirect demo

--- full_p940.mk
+++ full_p940.mk
@@ -23,7 +23,7 @@
  
 # Torch
 PRODUCT_PACKAGES := \
-    Torch WiFiDirectDemo
+    Torch
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
