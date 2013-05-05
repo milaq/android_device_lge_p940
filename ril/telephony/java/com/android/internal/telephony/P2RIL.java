b7f0273d4c265369d57bc7dac8bf2316de7a9772
milaq <micha.laqua@gmail.com>
2013-05-06 00:05:11 +0200

    ril: update ril extensions this is borrowed from p880; it seems the p880 ril is much closer to the p940 one than the p760s. see https://github.com/CyanogenMod/android_device_lge_p880/blob/cm-10.1/ril/telephony/java/com/android/internal/telephony/X3RIL.java
    it includes a custom queryCallForwardStatus and a RADIO_STATE_CHANGED hack. this will fix the following:
    * aeroplane mode (takes a bit to reassociate with celltowers but it definately works now)
    * call forwarding settings

--- ril/telephony/java/com/android/internal/telephony/P2RIL.java
+++ ril/telephony/java/com/android/internal/telephony/P2RIL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,83 +18,27 @@ package com.android.internal.telephony;
 import static com.android.internal.telephony.RILConstants.*;
 
 import android.content.Context;
-import android.os.AsyncResult;
 import android.os.Message;
 import android.os.Parcel;
 
 import android.telephony.PhoneNumberUtils;
-import android.telephony.PhoneStateListener;
-import android.telephony.TelephonyManager;
-
-import android.util.Log;
 
 public class P2RIL extends RIL implements CommandsInterface {
 
     public P2RIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
-        PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
-            @Override
-            public void onCallStateChanged(int state, String incomingNumber) {
-                /* Higher state wins, unless going back to idle */
-                if (state == TelephonyManager.CALL_STATE_IDLE || state > mCallState)
-                    mCallState = state;
-            }
-        };
-
-        // register for phone state notifications.
-        ((TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE))
-            .listen(mPhoneStateListener,
-                    PhoneStateListener.LISTEN_CALL_STATE);
     }
-
-    protected int mCallState = TelephonyManager.CALL_STATE_IDLE;
-
-    private int RIL_REQUEST_HANG_UP_CALL = 0xce;
-
-    /* We're not actually changing REQUEST_GET_IMEI, but it's one
-       of the first requests made after enabling the radio, and it
-       isn't repeated while the radio is on, so a good candidate to
-       inject initialization ops */
-
-    @Override
-    public void
-    dial(String address, int clirMode, UUSInfo uusInfo, Message result) {
-        super.dial(address, clirMode, uusInfo, result);
-
-        // RIL_REQUEST_LGE_CPATH
-        RILRequest rrLSL = RILRequest.obtain(
-                0x12a, null);
-        rrLSL.mp.writeInt(1);
-        rrLSL.mp.writeInt(1);
-        send(rrLSL);
-    }
-
-    public void
-    acceptCall (Message result) {
-        super.acceptCall(result);
-
-        // RIL_REQUEST_LGE_CPATH
-        RILRequest rrLSL = RILRequest.obtain(
-                0x12a, null);
-        rrLSL.mp.writeInt(1);
-        rrLSL.mp.writeInt(1);
-        send(rrLSL);
-    }
-
 
     @Override
     public void
     getIMEI(Message result) {
         //RIL_REQUEST_LGE_SEND_COMMAND
-        // Use this to bootstrap a bunch of internal variables
         RILRequest rrLSC = RILRequest.obtain(
-                0x142, null);
+                0x113, null);
         rrLSC.mp.writeInt(1);
-        rrLSC.mp.writeInt(0);
+        rrLSC.mp.writeInt(1);
         send(rrLSC);
 
-
-        // The original (and unmodified) IMEI request
         RILRequest rr = RILRequest.obtain(RIL_REQUEST_GET_IMEI, result);
 
         if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
@@ -104,32 +48,30 @@ public class P2RIL extends RIL implements CommandsInterface {
 
     @Override
     public void
-    hangupWaitingOrBackground (Message result) {
-        RILRequest rr = RILRequest.obtain(mCallState == TelephonyManager.CALL_STATE_OFFHOOK ?
-                                        RIL_REQUEST_HANGUP_WAITING_OR_BACKGROUND :
-                                        RIL_REQUEST_HANG_UP_CALL,
-                                        result);
+    queryCallForwardStatus(int cfReason, int serviceClass,
+                String number, Message response) {
+        RILRequest rr
+            = RILRequest.obtain(RIL_REQUEST_QUERY_CALL_FORWARD_STATUS, response);
 
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+        rr.mp.writeInt(2); // 2 is for query action, not in use anyway
+        rr.mp.writeInt(cfReason);
+        if (serviceClass == 0)
+            serviceClass = 255;
+        rr.mp.writeInt(serviceClass);
+        rr.mp.writeInt(PhoneNumberUtils.toaFromString(number));
+        rr.mp.writeString(number);
+        rr.mp.writeInt (0);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest)
+                + " " + cfReason + " " + serviceClass);
 
         send(rr);
     }
 
-    private static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                    + Character.digit(s.charAt(i+1), 16));
-        }
-        return data;
-    }
-
     static final int RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE = 1050;
     static final int RIL_UNSOL_LGE_XCALLSTAT = 1053;
-    static final int RIL_UNSOL_LGE_SIM_STATE_CHANGED = 1060;
-    static final int RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW = 1061;
     static final int RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC = 1074;
+    static final int RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW = 1061;
 
     @Override
     protected void
@@ -139,11 +81,10 @@ public class P2RIL extends RIL implements CommandsInterface {
         int response = p.readInt();
 
         switch(response) {
-            case RIL_UNSOL_ON_USSD: ret =  responseStrings(p); break;
+            case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED: ret =  responseVoid(p); break;
+            case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_XCALLSTAT: ret =  responseVoid(p); break;
             case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC: ret =  responseVoid(p); break;
-            case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE: ret =  responseVoid(p); break;
-            case RIL_UNSOL_LGE_SIM_STATE_CHANGED:
             case RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW: ret =  responseVoid(p); break;
             default:
                 // Rewind the Parcel
@@ -154,34 +95,20 @@ public class P2RIL extends RIL implements CommandsInterface {
                 return;
         }
         switch(response) {
-            case RIL_UNSOL_ON_USSD:
-                String[] resp = (String[])ret;
-
-                if (resp.length < 2) {
-                    resp = new String[2];
-                    resp[0] = ((String[])ret)[0];
-                    resp[1] = null;
+            case RIL_UNSOL_RESPONSE_RADIO_STATE_CHANGED:
+                /* has bonus radio state int */
+                RadioState newState = getRadioStateFromInt(p.readInt());
+                p.setDataPosition(dataPosition);
+                super.processUnsolicited(p);
+                if (RadioState.RADIO_ON == newState) {
+                    setNetworkSelectionModeAutomatic(null);
                 }
-                if (resp[1].length()%2 == 0 && resp[1].matches("[0-9A-F]+")) {
-                    try { 
-                        resp[1] = new String(hexStringToByteArray(resp[1]), "UTF-16");
-                    } catch (java.io.UnsupportedEncodingException uex) { 
-                        // encoding not supported, should never get here 
-                    } catch (java.io.IOException iox) { 
-                        // you will get here if the original sequence wasn't UTF-8 or ASCII 
-                    } 
-                }
-                if (RILJ_LOGD) unsljLogMore(response, resp[0]);
-                if (mUSSDRegistrant != null) {
-                    mUSSDRegistrant.notifyRegistrant(
-                        new AsyncResult (null, resp, null));
-                }
+                return;
+            case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE:
+            case RIL_UNSOL_LGE_XCALLSTAT:
+            case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC:
+                if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
                 break;
-            case 1080: // RIL_UNSOL_LGE_FACTORY_READY (NG)
-                /* Adjust request IDs */
-                RIL_REQUEST_HANG_UP_CALL = 206;
-                break;
-            case RIL_UNSOL_LGE_SIM_STATE_CHANGED:
             case RIL_UNSOL_LGE_SIM_STATE_CHANGED_NEW:
                 if (RILJ_LOGD) unsljLog(response);
 
@@ -189,12 +116,7 @@ public class P2RIL extends RIL implements CommandsInterface {
                     mIccStatusChangedRegistrants.notifyRegistrants();
                 }
                 break;
-            case RIL_UNSOL_LGE_BATTERY_LEVEL_UPDATE:
-            case RIL_UNSOL_LGE_XCALLSTAT:
-            case RIL_UNSOL_LGE_SELECTED_SPEECH_CODEC:
-                if (RILJ_LOGD) riljLog("sinking LGE request > " + response);
         }
-
     }
 
 }
