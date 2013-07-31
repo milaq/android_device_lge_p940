375eb3fb6897ca0b51199b39d1c901e30e3f3d07
milaq <micha.laqua@gmail.com>
2013-07-31 02:53:16 +0200

    allow radio to access some props

---
+++ include/device_perms.h
@@ -0,0 +1,4 @@
+#define PROPERTY_PERMS_APPEND \
+    { "watchdog.reset_progress",       AID_RADIO,    AID_RADIO }, \
+    { "net.interfaces.defaultroute",   AID_RADIO,    AID_RADIO }, \
+    { "persist.wifi.mac",              AID_RADIO,    AID_RADIO },
