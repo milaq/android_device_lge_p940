CyanogenMod/android_device_lge_p760

selinux: Add device-specific rules
Change-Id: I69952d49ef0b48d634debc5ac5bc7c2eeea23903

BoardConfig.mk
@@ -95,3 +95,16 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p760/bluetooth
 
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p760/recovery-gfx.c
 BOARD_CHARGER_ENABLE_SUSPEND := true
+
+ifeq ($(HAVE_SELINUX),true)
+
+BOARD_SEPOLICY_DIRS := \
+    device/lge/p760/selinux
+
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    pvrsrvinit.te \
+    device.te \
+    domain.te
+
+endif

milaq/android_device_lge_p940

BoardConfig.mk
@@ -113,6 +113,15 @@ TARGET_KERNEL_MODULES := KERNEL_SGX_MODULES
 
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 
+BOARD_SEPOLICY_DIRS := \
+    device/lge/p940/selinux
+
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    pvrsrvinit.te \
+    device.te \
+    domain.te
+
 BOARD_RECOVERY_ALWAYS_WIPES := true
 TARGET_RECOVERY_INITRC := device/lge/p940/recovery/init.rc
 BOARD_UMS_LUNFILE := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun0/file"